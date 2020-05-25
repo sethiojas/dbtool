a3636829f03fc4e1d29c8300f01f8d57ad4bb16e
Ojas Sethi <sethi.ojas@gmail.com>
2020-05-25 14:02:23 +0530

    Added docstrings

--- helper.py
+++ helper.py
@@ -3,6 +3,11 @@ import sys
 import datetime
 
 def format_value(col_value):
+    '''
+    param :col_value: - value of a column in a row
+    returns           - column value formatted in a way which is
+                        suitable for insertion into database
+    '''
     if type(col_value) == datetime.datetime:
         return f"'{str(col_value.date())}'"
     elif type(col_value) == str:
@@ -11,6 +16,16 @@ def format_value(col_value):
         return col_value
 
 def retrieve_path_and_dbname(arguments):
+    '''
+    param :arguments: - sys.argv of dbtool.py
+    returns           - [tuple] path to spreadsheet, name of database
+
+    handles following Exceptions:
+        * path to file is not specified
+        * too many input arguments
+        * path does not exists/path is not a file
+        * format of file is not supported
+    '''
     try:
         supported_formats = tuple({'xlsx', 'xlsm', 'xlxt', 'xltm'})
 
@@ -37,10 +52,14 @@ Supported formats are: .xlsx,.xlsm,.xltx,.xltm''')
         print(e)
         sys.exit(1)
 
-def get_datatypes(iter):
+def get_datatypes(row):
+    '''
+    param :iter:  - single row of data
+    returns       - [list] datatype of each column for sql query
+    '''
     datatypes = list()
    
-    for item in next(iter):  
+    for item in row:  
         type_of_item = type(item)
         
         if type_of_item == int:
