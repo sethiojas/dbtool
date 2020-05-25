49279bf0fc5347ab8a0aa1933578185c71e2d25e
Ojas Sethi <sethi.ojas@gmail.com>
2020-05-25 13:35:45 +0530

    Code refactor

--- dbtool.py
+++ dbtool.py
@@ -4,7 +4,7 @@ import sqlite3
 import sys
 import os
 
-def foo(col_value):
+def format_value(col_value):
     if type(col_value) == datetime.datetime:
         return f"'{str(col_value.date())}'"
     elif type(col_value) == str:
@@ -12,13 +12,51 @@ def foo(col_value):
     else:
         return col_value
 
-path = sys.argv[1]
-db_name = os.path.split(path)[1].split(".")[0]
+def retrieve_path_and_dbname(arguments):
+    try:
+        supported_formats = tuple({'xlsx', 'xlsm', 'xlxt', 'xltm'})
 
-conn = sqlite3.connect(db_name + ".db")
-cur = conn.cursor()
+        if (len(arguments) < 2):
+            raise Exception('Path to file not specified')
+        elif (len(arguments) > 2):
+            raise Exception('Too many input arguments')
 
-workbook = load_workbook('test.xlsx', read_only=True)
+        path = arguments[1]
+        
+        if(not os.path.isfile(path)):
+            raise Exception('Path does not exists or is not of a file')
+        
+        filename = os.path.split(path)[1]
+        dbname, extension = filename.split(".")
+
+        if (extension not in supported_formats):
+            raise Exception(f'''Format not supported '{extension}'.
+Supported formats are: .xlsx,.xlsm,.xltx,.xltm''')
+
+        return (path, dbname + '.db')
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def get_datatypes(iter):
+    datatypes = list()
+   
+    for item in next(sheet_iter):  
+        type_of_item = type(item)
+        
+        if type_of_item == int:
+            datatypes.append('INTEGER')
+        elif type_of_item == float:
+            datatypes.append('REAL')
+        else:
+            datatypes.append('STRING')
+    
+    return datatypes
+
+path, db_name = retrieve_path_and_dbname(sys.argv)
+
+workbook = load_workbook(path, read_only=True)
 table_name = workbook.sheetnames[0].upper()
 sheet = workbook.active
 sheet_iter = sheet.values
@@ -26,20 +64,14 @@ sheet_iter = sheet.values
 column_names = tuple(next(sheet_iter))
 column_names = tuple(map(lambda elem: elem.upper().replace(" ", "_"), column_names))
 
-datatypes = list()
-for item in next(sheet_iter):
-    type_of_item = type(item)
-    
-    if type_of_item == int:
-        datatypes.append('INTEGER')
-    elif type_of_item == float:
-        datatypes.append('REAL')
-    else:
-        datatypes.append('STRING')
+datatypes = get_datatypes(sheet_iter)
 
 create_table_query = f'create table {table_name} ('
 joined_colName_and_types = ', '.join(f"{i} {j}" for (i,j) in zip(column_names, datatypes))
 create_table_query += joined_colName_and_types + ');'
+
+conn = sqlite3.connect(db_name)
+cur = conn.cursor()
 
 cur.execute(create_table_query)
 
@@ -47,7 +79,7 @@ sheet_iter = sheet.values
 next(sheet_iter)
 try:
     while(True):
-        row = map(lambda x: foo(x), next(sheet_iter))
+        row = map(lambda x: format_value(x), next(sheet_iter))
         insert_query = f'insert into {table_name} values('
         joined_rows  = ', '.join(f'{i}' for i in row)
         insert_query += joined_rows + ');'
